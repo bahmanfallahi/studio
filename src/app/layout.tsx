37f11589b56e28ef06699227947f9daef13f607e
bahman fallahi <bahman.f.behtash@gmail.com>
2025-08-30 10:42:58 +0000

    این خطای **Hydration Error** در Next.js است که یعنی محتوای HTML که سرور

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body">
+      <body className="font-body" suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster />
